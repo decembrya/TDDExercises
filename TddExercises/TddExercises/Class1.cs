1f8ef6736ac28c2fcdbba94e858a5709f24f9d17
Iulia Dormenco <Iulia.Dormenco@webbeds.com>
2021-04-09 11:44:18 +0300

    add solution and exercise setup

---
+++ TddExercises/TddExercises/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace TddExercises
+{
+    //Challenge: +
+    //a functionality that converts arabic numbers to roman numerals
+    //input will be int 
+    //output will be string
+    //examples:
+    //1-> I
+    //11-> XI
+    public class Class1
+    {
+    }
+}
