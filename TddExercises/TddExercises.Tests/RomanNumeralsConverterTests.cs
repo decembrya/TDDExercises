e6e97f1d217fdfbf0635306fa10adba99165864d
Ciprian Stingu <ciprian.stingu@webbeds.com>
2021-04-13 14:01:44 +0200

    case 5 & test 0

--- TddExercises/TddExercises.Tests/RomanNumeralsConverterTests.cs
+++ TddExercises/TddExercises.Tests/RomanNumeralsConverterTests.cs
@@ -4,37 +4,18 @@ using Xunit;
 namespace TddExercises.Tests
 {
     //Red-Green-Refactor
-   
     public class RomanNumeralsConverterTests
     {
-        [Fact]
-        public void When_InputIsOne_Return_I()
+        [InlineData(0, "")]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [Theory]
+        public void When_InputIsOne_Return_I(int input, string expected)
         {
-            var input = 1;
-
             var result = new RomanNumeralsConverter().Convert(input);
-
-            result.Should().Be("I");
-        }
-        
-        [Fact]
-        public void When_InputIsTwo_Return_II()
-        {
-            var input = 2;
-
-            var result = new RomanNumeralsConverter().Convert(input);
-
-            result.Should().Be("II");
-        }
-
-        [Fact]
-        public void When_InputIsTwo_Return_IV()
-        {
-            var input = 4;
-
-            var result = new RomanNumeralsConverter().Convert(input);
-
-            result.Should().Be("IV");
+            result.Should().Be(expected);
         }
     }
 }
--- TddExercises/TddExercises/RomanNumeralsConverter.cs
+++ TddExercises/TddExercises/RomanNumeralsConverter.cs
@@ -1,4 +1,6 @@
-﻿namespace TddExercises
+﻿using System.Collections.Generic;
+
+namespace TddExercises
 {
     //Challenge: +
     //a functionality that converts arabic numbers to roman numerals
@@ -9,14 +11,12 @@
     //11-> XI
     public class RomanNumeralsConverter
     {
+        private Dictionary<int, string> dictionary = new Dictionary<int, string>() { { 1, "I" }, { 2, "II" }, { 4, "IV" }, { 5, "V" } };
+
         public string Convert(int input)
         {
-            if (input == 1)
-            {
-                return "I";
-            }
+            return dictionary[input];
 
-            return "II";
         }
     }
 }
