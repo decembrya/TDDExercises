877f8763d74f8d2130b9e073f47595da881ecb92
decembrya <iuliaolteanu12@gmail.com>
2021-04-13 12:11:04 +0300

    remove comment

--- TddExercises/TddExercises.Tests/UnitTest1.cs
+++ TddExercises/TddExercises.Tests/UnitTest1.cs
@@ -3,9 +3,7 @@ using Xunit;
 namespace TddExercises.Tests
 {
     //Red-Green-Refactor
-    //Detroit (Classicist) school of TDD - inside-out design
-    //London (Mockist) school of TDD - outside-in design
-
+   
     public class UnitTest1
     {
         [Fact]
